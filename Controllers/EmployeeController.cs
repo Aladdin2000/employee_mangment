c7a9a665d6bdad275a89a6919af169b3ffbaf8ac
Aladdin2000 <alaaeldeenmohamed3@gmail.com>
2026-03-14 03:21:10 +0200

    Final update - complete employee management system

--- Controllers/EmployeeController.cs
+++ Controllers/EmployeeController.cs
@@ -9,7 +9,6 @@ namespace employees_mangment.Controllers
     {
         private ApplicationDbContext db = new ApplicationDbContext();
 
-        // GET: Employee
         public ActionResult Index()
         {
             var employees = db.Employees
@@ -18,7 +17,6 @@ namespace employees_mangment.Controllers
             return View(employees);
         }
 
-        // GET: Employee/Create
         public ActionResult Create()
         {
             var properties = db.PropertyDefinitions.ToList();
@@ -26,27 +24,89 @@ namespace employees_mangment.Controllers
             return View(new employees());
         }
 
-        // POST: Employee/Create
         [HttpPost]
-        public ActionResult Create(employees employee, string[] propertyValues)
+        public ActionResult Create(employees employee, FormCollection form)
         {
             if (ModelState.IsValid)
             {
                 var properties = db.PropertyDefinitions.ToList();
+                db.Employees.Add(employee);
+                db.SaveChanges();
                 for (int i = 0; i < properties.Count; i++)
                 {
-                    employee.PropertyValues.Add(new EmployeePropertyValue
+                    var value = form["prop_" + i];
+                    db.EmployeePropertyValues.Add(new EmployeePropertyValue
                     {
+                        EmployeeId = employee.Id,
                         PropertyDefinitionId = properties[i].Id,
-                        Value = propertyValues != null && propertyValues.Length > i
-                            ? propertyValues[i] : ""
+                        Value = value ?? ""
                     });
                 }
-                db.Employees.Add(employee);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.Properties = db.PropertyDefinitions.ToList();
+            return View(employee);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var employee = db.Employees
+                .Include(e => e.PropertyValues)
+                .FirstOrDefault(e => e.Id == id);
+            var properties = db.PropertyDefinitions.ToList();
+            ViewBag.Properties = properties;
+            return View(employee);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(int id, employees employee, FormCollection form)
+        {
+            if (ModelState.IsValid)
+            {
+                var properties = db.PropertyDefinitions.ToList();
+                var existing = db.Employees
+                    .Include(e => e.PropertyValues)
+                    .FirstOrDefault(e => e.Id == id);
+
+                existing.Code = employee.Code;
+                existing.Name = employee.Name;
+
+                foreach (var pv in existing.PropertyValues.ToList())
+                {
+                    db.EmployeePropertyValues.Remove(pv);
+                }
+
+                for (int i = 0; i < properties.Count; i++)
+                {
+                    var value = form["prop_" + i];
+                    db.EmployeePropertyValues.Add(new EmployeePropertyValue
+                    {
+                        EmployeeId = id,
+                        PropertyDefinitionId = properties[i].Id,
+                        Value = value ?? ""
+                    });
+                }
                 db.SaveChanges();
                 return RedirectToAction("Index");
             }
             return View(employee);
+        }
+
+    
+        public ActionResult Delete(int id)
+        {
+            var employee = db.Employees
+                .Include(e => e.PropertyValues)
+                .FirstOrDefault(e => e.Id == id);
+
+            foreach (var pv in employee.PropertyValues.ToList())
+            {
+                db.EmployeePropertyValues.Remove(pv);
+            }
+            db.Employees.Remove(employee);
+            db.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
--- Controllers/PropertyDefinitionController.cs
+++ Controllers/PropertyDefinitionController.cs
@@ -2,27 +2,23 @@
 using employees_mangment.Models;
 using System.Linq;
 
-
 namespace employees_mangment.Controllers
 {
     public class PropertyDefinitionController : Controller
     {
         private ApplicationDbContext db = new ApplicationDbContext();
 
-        // GET: PropertyDefinition
         public ActionResult Index()
         {
             var properties = db.PropertyDefinitions.ToList();
             return View(properties);
         }
 
-        // GET: PropertyDefinition/Create
         public ActionResult Create()
         {
             return View();
         }
 
-        // POST: PropertyDefinition/Create
         [HttpPost]
         public ActionResult Create(PropertyDefinition property)
         {
@@ -33,6 +29,39 @@ namespace employees_mangment.Controllers
                 return RedirectToAction("Index");
             }
             return View(property);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var property = db.PropertyDefinitions.Find(id);
+            return View(property);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(int id, PropertyDefinition property)
+        {
+            if (ModelState.IsValid)
+            {
+                var existing = db.PropertyDefinitions.Find(id);
+                existing.Name = property.Name;
+                existing.Type = property.Type;
+                existing.IsRequired = property.IsRequired;
+                existing.DropdownOptions = property.DropdownOptions;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(property);
+        }
+
+        public ActionResult Delete(int id)
+        {
+            var property = db.PropertyDefinitions.Find(id);
+            if (property != null)
+            {
+                db.PropertyDefinitions.Remove(property);
+                db.SaveChanges();
+            }
+            return RedirectToAction("Index");
         }
     }
 }
